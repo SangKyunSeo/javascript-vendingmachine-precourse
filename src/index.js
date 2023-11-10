c4f5a5366dfd5caf66ab2f66a1b6da0d2627294b
서상균 <SGO1022322@shinhan.com>
2023-11-10 18:03:30 +0900

    feat: 자바스크립트 공부를 위한 자판기 프로젝트 구현중

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import PrintManageProduct from './printManageProduct.js'
+
+export const app = document.getElementById('app');
+const printManageProduct = new PrintManageProduct();
+
+let textNode = '';
+
+class Common{
+    generateSecionTab(){
+        const sectionTab = document.createElement('div');
+        sectionTab.id = 'tabContent';
+        return sectionTab;
+    }
+
+    generateManageProductButton(){
+        const manageProductBtn = document.createElement('button');
+        manageProductBtn.id = 'product-purchase-menu';
+        textNode = document.createTextNode('상품 관리');
+        manageProductBtn.appendChild(textNode);
+        return manageProductBtn;
+    }
+
+    generateInsertBalanceButton(){
+        const insertBalanceBtn = document.createElement('button');
+        insertBalanceBtn.id = 'vending-machine-manage-menu';
+        textNode = document.createTextNode('잔돈 충전');
+        insertBalanceBtn.appendChild(textNode);
+        return insertBalanceBtn;
+    }
+
+    generateBuyProductButton(){
+        const buyProductButton = document.createElement('button');
+        buyProductButton.id = 'product-add-menu';
+        textNode = document.createTextNode('상품 구매');
+        buyProductButton.appendChild(textNode);
+        return buyProductButton;
+    }
+
+    generateCommonTab(){
+        let commonTab = this.generateSecionTab();
+        app.appendChild(commonTab);
+        commonTab.appendChild(this.generateManageProductButton());
+        commonTab.appendChild(this.generateInsertBalanceButton());
+        commonTab.appendChild(this.generateBuyProductButton());
+    }
+}
+
+const common = new Common();
+common.generateCommonTab();
+
+document.querySelector('#product-purchase-menu').addEventListener('click',()=>{
+    printManageProduct.appendManageProduct();
+});
+
---
+++ src/printManageProduct.js
@@ -0,0 +1,84 @@
+import {app} from './index.js'
+
+export default class PrintManageProduct{
+
+    // 상품 관리에 대한 부분을 감싸는 div 요소 생성
+    generateManageProductSecion(){
+        const manageProductSection = document.createElement('div');
+        manageProductSection.id = 'product-manage-section';
+        return manageProductSection;
+    }
+
+    // 상품 정보 입력 폼 요소 생성
+    generateManageProductForm(){
+        const manageProductForm = document.createElement('form');
+        manageProductForm.id = 'product-input-form';
+        return manageProductForm;
+    }
+
+    // 상품명 입력 요소 생성
+    generateInputProductName(){
+        const inputProductName = document.createElement('input');
+        inputProductName.id = 'product-name-input';
+        inputProductName.placeholder = '상품명';
+        return inputProductName;
+    }
+
+    // 상품 가격 입력 요소 생성
+    generateInputProductPrice(){
+        const inputProductPrice = document.createElement('input');
+        inputProductPrice.id = 'product-price-input';
+        inputProductPrice.placeholder = '가격';
+        return inputProductPrice;
+    }
+
+    // 상품 수량 입력 요소 생성
+    generateInputProductAmount(){
+        const inputProductQuantity = document.createElement('input');
+        inputProductQuantity.id = 'product-quantity-input';
+        inputProductQuantity.placeholder = '수량';
+        return inputProductQuantity;
+    }
+
+    // 추가하기 버튼 요소 생성
+    generateSubmitProductAddButton(){
+        const submitProductAddButton = document.createElement('button');
+        const textNode = document.createTextNode('추가하기');
+        submitProductAddButton.appendChild(textNode);
+        submitProductAddButton.id = 'product-add-button';
+        return submitProductAddButton;
+    }
+    
+    // 상품 추가 기능 요소 생성시 그 상태 로컬스토리지에 저장
+    saveLocalStorageOfManageProductState(){
+        localStorage.setItem('isGenerateManageProductSection', true);
+    }
+
+    // 이미 상품 추가 기능 요소가 생성되었는지 체크
+    checkGeneratedManageProduct(){
+        if(localStorage.getItem('isGenerateManageProductSection') === 'true') return true;
+        return false;
+    }
+
+    // 상품 관리 탭 클릭 시 상품 추가 기능 요소를 감싸는 요소 생성
+    appendManageProductSection(){
+        let manageProductSection = this.generateManageProductSecion();
+        let manageProductForm = this.generateManageProductForm();
+        app.appendChild(manageProductSection);
+        manageProductSection.appendChild(manageProductForm);
+        this.saveLocalStorageOfManageProductState();
+        return manageProductForm;
+    }
+
+    // 상품 관리 탭 클릭 시 상품 추가 기능 요소 생성
+    appendManageProduct(){
+        if(!this.checkGeneratedManageProduct()){
+            let manageProductForm = this.appendManageProductSection();
+            manageProductForm.appendChild(this.generateInputProductName());
+            manageProductForm.appendChild(this.generateInputProductPrice());
+            manageProductForm.appendChild(this.generateInputProductAmount());
+            manageProductForm.appendChild(this.generateSubmitProductAddButton());
+        }
+    }
+}
+
